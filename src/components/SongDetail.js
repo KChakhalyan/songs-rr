b2ea33514f3346a5ea0b3783b52d93ba7a605b17
Karen <karenchakhalyan23@gmail.com>
2021-04-03 22:58:29 +0400

    some fixes

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -5,11 +5,19 @@ const SongDetail = ({ song }) => {
 	if (!song) {
 		return <div>Select a song</div>;
 	}
-	return <div>{song.title}</div>;
+	return (
+		<div>
+			<h3>Song Details</h3>
+			<p>
+				<strong>Title: {song.title}</strong>
+			</p>
+			<p>Duration: {song.duration}</p>
+		</div>
+	);
 };
 
 const mapStateToProps = (state) => {
-	return { song: state.slectedSong };
+	return { song: state.selectedSong };
 };
 
 export default connect(mapStateToProps)(SongDetail);
